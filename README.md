1b0e81582e0f95dbb830dbbc01dfcf4ccea3d494
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-12-06 00:31:03 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# gatsby-remark-drupal
+
+Provides support for markdown preprocessing to Drupal body fields.
+
+* Creates a new `text/markdown` field for drupal body fields.
+* Map drupal relative paths to previously downloaded and cached images by `gatsby-data-source`.
+
+NOTE: To make sure your Drupal site expose markdown use the Toast UI Editor integration for Drupal provided by the [tui_editor](https://www.drupal.org/project/tui_editor) module.
+
+## Install
+
+`npm install --save gatsby-drupal-remark`
+
+## How to use
+
+### Default configuration
+```javascript
+      // In your gatsby-config.js
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-drupal',
+```
+
+### Custom configuration
+```javascript
+      // In your gatsby-config.js
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-drupal`,
+            options: {
+              nodes: [`article`,`page`]
+            }
+          }
+        ]
+      }
+```
+## Options
+| Name | Default | Description |
+| --- | --- | --- |
+| `nodes` | [`article`,`page`] | The Drupal `node` types to process.
---
+++ gatsby-node.js
@@ -0,0 +1,71 @@
+
+const _camelCase = require('lodash/camelCase');
+const path = require('path');
+const crypto = require('crypto');
+const digest = str => crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex');
+
+exports.onCreateNode = ({
+    node,
+    getNodes,
+    actions
+}, pluginOptions) => {
+
+    if (node.internal.owner !== 'gatsby-source-drupal') {
+        return
+    }
+
+    if (!pluginOptions.hasOwnProperty("nodes")) {
+        pluginOptions.nodes = [`article`,`page`];
+    }
+
+    const node_types = pluginOptions.nodes.map((type) => {
+        return `node__${type}`;
+    });
+
+    if (node_types.includes(node.internal.type)) {
+
+        const {
+            createNode,
+            createNodeField
+        } = actions;
+
+        let content = node.body.value;
+        const textNode = {
+            id: `${node.id}-MarkdownBody`,
+            parent: node.id,
+            dir: path.resolve('./'),
+            internal: {
+                type: _camelCase(`${node.internal.type}MarkdownBody`),
+                mediaType: 'text/markdown'
+            }
+        };
+
+        const inlineImageRegExp = /\(\/sites[^)]+\)/gi;
+        const nodeImages = content.match(inlineImageRegExp);
+        if (nodeImages) {
+            const nodes = getNodes();
+            nodeImages.forEach((element) => {
+                const nodeImage = element.slice(1, -1);
+                const nodeImageCached = nodes.find(contentNode => (contentNode.internal.type === 'File' && contentNode.internal.description.includes(nodeImage)));
+                if (nodeImageCached) {
+                    console.log(`mapping ${nodeImage}`);
+                    content = content.replace(new RegExp(nodeImage, 'g'), nodeImageCached.relativePath);
+                }
+            });
+        }
+
+        textNode.internal.content = content;
+        textNode.internal.contentDigest = digest(content);
+        createNode(textNode);
+        createNodeField({
+            node,
+            name: 'markdownBody___NODE',
+            value: textNode.id
+        });
+    }
+
+    return;
+}
